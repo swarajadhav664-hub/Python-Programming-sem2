6b2c2af11fd1de8dfd1f467fcddeb59be21a3d76
swarajadhav664-hub <swarajadhav664@gmail.com>
2026-02-17 13:20:31 +0530

    Create nested.py

---
+++ Exp3/nested.py
@@ -0,0 +1,14 @@
+#experiment 3 1st code
+"""
+Created on Tue Feb 17 12:26:52 2026
+
+@author: swaranjali jadhav
+
+"""
+n=int(input("Enter numbers of rows:"))
+
+for i in range(1,n + 1):
+    for j in range(1, i + 1):
+        print(j, end="")  
+    print()
+    

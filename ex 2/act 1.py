a167760dc14b7d2af57edb40fc27cf757a9a5237
swarajadhav664-hub <swarajadhav664@gmail.com>
2026-04-28 13:45:22 +0530

    Add files via upload

---
+++ ex 2/act 1.py
@@ -0,0 +1,15 @@
+# -*exp.2 act 1
+"""
+Created on Tue Mar 17 12:41:05 2026
+
+@author: swaranjali jadhav
+""" 
+
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Entry Allowed")
+else:
+    print("Entry Denied")
+

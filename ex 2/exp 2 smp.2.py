46d1b55f9ee33e80ed279b18f188195194bce7f8
swarajadhav664-hub <swarajadhav664@gmail.com>
2026-04-28 13:48:08 +0530

    Add files via upload

---
+++ ex 2/exp 2 smp.2.py
@@ -0,0 +1,11 @@
+# exp 2 sample2
+"""
+Created on Tue Mar 17 13:09:14 2026
+
+@author: swaranjali jadhav
+"""
+n = int(input("Enter number:"))
+fact = 1
+for i in range(1, n + 1):
+  fact = fact * i
+print("Factorial:", fact)

d3ccd236f86830066b3f5f7b68b669414439772f
swarajadhav664-hub <swarajadhav664@gmail.com>
2026-04-28 13:47:37 +0530

    Add files via upload

---
+++ ex 2/act 3.py
@@ -0,0 +1,13 @@
+# -*exp.2 act 3
+"""
+Created on Tue Mar 17 12:41:05 2026
+
+@author: swaranjali jadhav
+""" 
+
+salary = int(input("Enter your salary: "))
+
+if salary >= 25000:
+    print("Loan Approved")
+else:
+    print("Loan Rejected") 

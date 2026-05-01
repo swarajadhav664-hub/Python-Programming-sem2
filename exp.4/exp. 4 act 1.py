37133ec3b4ab1248c490ebdaac5d4bb4120d0956
swarajadhav664-hub <swarajadhav664@gmail.com>
2026-05-01 09:52:08 +0530

    Add files via upload

---
+++ exp.4/exp. 4 act 1.py
@@ -0,0 +1,18 @@
+# exp.4 activity 1
+"""
+Created on Tue Mar 10 13:35:24 2026
+
+@author: swaranjali jadhav
+"""
+
+# Enter marks of students
+marks = [65, 78, 90, 55, 88]
+
+# Find average
+average = sum(marks) / len(marks)
+
+# Find topper
+topper = max(marks)
+
+print("Average Marks:", average)
+print("Topper Marks:", topper)

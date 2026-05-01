eaddf51eff79fa79093a3140526d6d181307b1b0
swarajadhav664-hub <swarajadhav664@gmail.com>
2026-05-01 14:58:46 +0530

    Add files via upload

---
+++ 6/act 2.py
@@ -0,0 +1,15 @@
+
+
+#exp.6 act 2
+"""
+Created on Tue Mar 24 13:15:54 2026
+
+@author: swaranjali jadhav
+"""
+# Store expenses
+expenses = [100, 200, 150, 300, 250]
+
+# Calculate total
+total = sum(expenses)
+
+print("Total Monthly Expense =", total) 
